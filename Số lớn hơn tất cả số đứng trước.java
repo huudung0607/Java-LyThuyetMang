98b8b4741eb949e799efe2bcd27c8d07eaa34bf4
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-08-26 09:42:06 +0700

    Create Số lớn hơn tất cả số đứng trước.java

---
+++ Số lớn hơn tất cả số đứng trước.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Main {
+    private static int mod = 1000000007;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int []a = new int[n];
+        for(int i = 0 ; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int max = -1;
+        for(int i = 0;i < n ;i++){
+            if(a[i] > max){
+                max = a[i];
+                System.out.print(max + " ");
+            }
+        }
+    }
+}
