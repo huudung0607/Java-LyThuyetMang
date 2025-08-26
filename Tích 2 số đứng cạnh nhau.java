e9d287cc0cb22e6dbd57cfbc2eba16748cc821cf
Huỳnh Hữu Dũng <huudung0607@gmail.com>
2025-08-26 09:40:17 +0700

    Create Tích 2 số đứng cạnh nhau.java

---
+++ Tích 2 số đứng cạnh nhau.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Main {
+    private static int mod = 1000000007;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int []a = new int[n];
+        for(int i = 0 ; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        for(int i = 0; i < n - 1;i ++){
+            System.out.print(a[i]*a[i + 1] + " ");
+        }
+    }
+}
